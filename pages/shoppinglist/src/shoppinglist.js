fd4683c491c8bbb5fe589d0e06ee1cd47b28bcd9
Victor Gabriel <victorrgabrielcs@gmail.com>
2024-08-26 21:43:38 -0300

    correção de bugs, tratamento de error

--- pages/shoppinglist/src/shoppinglist.js
+++ pages/shoppinglist/src/shoppinglist.js
@@ -9,16 +9,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const itemNameElement = document.getElementById("item-name");
 const itemAmountElement = document.getElementById("item-amount");
+const tableElement = document.querySelector("table");
 function addItems() {
-  createTableHead("Nome", "Quantidade");
+  createTableHead("Nome", "Quantidade", "Opções de Edição");
   createTableBody(itemNameElement.value, itemAmountElement.value);
   createTableFooter("Quantidade Total");
-
 }
 
-function createTableHead(col1, col2) {
-  const tableElement = document.querySelector("table");
-
+function createTableHead(col1, col2, col3) {
   if (tableElement.contains(document.querySelector("thead"))) {
     return;
   }
@@ -26,30 +24,50 @@ function createTableHead(col1, col2) {
   const trElement = document.createElement("tr");
   const thNameElement = document.createElement("th");
   const thAmountElement = document.createElement("th");
+  const thEditOptions = document.createElement("th");
 
   tableElement.appendChild(theadElement);
   theadElement.appendChild(trElement);
   trElement.appendChild(thNameElement);
   trElement.appendChild(thAmountElement);
+  trElement.appendChild(thEditOptions);
 
   thNameElement.setAttribute("scope", "col");
   thAmountElement.setAttribute("scope", "col");
+  thEditOptions.setAttribute("scope", "col");
   thNameElement.innerText = col1;
   thAmountElement.innerText = col2;
+  thEditOptions.innerText = col3;
 }
 
 function createTableBody(itemName, itemAmount) {
-  const tableElement = document.querySelector("table");
-
   let alternateMethod = () => {
     return document.createElement("tbody");
   };
+  try {
+    if (itemName === "") {
+      throw new Error("Nenhum item adicionado");
+    }
+
+    if (itemAmount === "" || itemAmount <= 0) {
+      throw new Error("Quantidade deve ser maior do que 0");
+    }
+  } catch (error) {
+    console.error(error.message);
+    alert(error.message);
+    return;
+  }
 
   if (tableElement.contains(document.querySelector("tbody"))) {
     const tbodyElement = document.querySelector("tbody");
     for (let trNames of tbodyElement.children) {
-      if (trNames.children[0].innerText === itemName) {
-        alert("Item já adicionado");
+      try {
+        if (trNames.children[0].innerText === itemName) {
+          throw new Error("Item já adicionado");
+        }
+      } catch (error) {
+        console.error(error.message);
+        alert(error.message);
         return;
       }
     }
@@ -62,50 +80,70 @@ function createTableBody(itemName, itemAmount) {
   const trElement = document.createElement("tr");
   const tdNemeElement = document.createElement("td");
   const tdAmountElement = document.createElement("td");
+  const tdEditOptions = document.createElement("td");
 
   tableElement.appendChild(tbodyElement);
   tbodyElement.appendChild(trElement);
   trElement.appendChild(tdNemeElement);
   trElement.appendChild(tdAmountElement);
+  trElement.appendChild(tdEditOptions);
 
   tdNemeElement.setAttribute("class", "col-name");
   tdAmountElement.setAttribute("class", "col-amount");
   tdNemeElement.innerText = itemName;
   tdAmountElement.innerText = itemAmount;
+  tdEditOptions.innerHTML = `<span class='delete material-symbols-outlined' onclick=''>delete</span> <span class='edit material-symbols-outlined'>edit</span>`;
+
+  tdEditOptions.onclick = function(event){
+    const deleteElementParent = event.target.parentElement.parentElement;
+
+    deleteElementParent.remove();
+  }
+
+  if (tableElement.contains(document.querySelector("tfoot"))) {
+    calculateTotalAmount();
+  }
 }
 
-function createTableFooter(col1){
-    const tableElement = document.querySelector("table");
+function createTableFooter(col1) {
+  if (tableElement.contains(document.querySelector("tfoot"))) {
+    return;
+  }
 
-    if(tableElement.contains(document.querySelector("tfoot"))){
-        const tfootElement = document.querySelector("tfoot");
-        const totalAmountElement = document.getElementById("total-amount");
-        let totalValue = Number(totalAmountElement.textContent)
-        for(let trAmounts of tfootElement.children){
-             totalValue += Number(trAmounts.children[1].textContent)
-        }
-        totalAmountElement.textContent = totalValue
-        return;
+  const tfooterElement = document.createElement("tfoot");
+  const trElement = document.createElement("tr");
+  const thElement = document.createElement("th");
+  const tdElement = document.createElement("td");
+  const tdElementEmpty = document.createElement("td");
+
+  tableElement.appendChild(tfooterElement);
+  tfooterElement.appendChild(trElement);
+  trElement.appendChild(thElement);
+  trElement.appendChild(tdElement);
+  trElement.appendChild(tdElementEmpty);
+
+  let totalValue = 0;
+
+  for (let trAmounts of tableElement.children[2].children) {
+    totalValue += Number(trAmounts.children[1].textContent);
+  }
+
+  thElement.setAttribute("scope", "row");
+  tdElement.setAttribute("id", "total-amount");
+  thElement.innerText = col1;
+  tdElement.innerText = totalValue;
+  tdElementEmpty.innerText = "";
+}
+
+function calculateTotalAmount() {
+  const tbodyElement = document.querySelector("tbody");
+  const tfootElement = document.querySelector("tfoot");
+  const totalAmountElement = document.getElementById("total-amount");
+  let totalValue = Number(totalAmountElement.textContent);
+  for (let trAmounts of tbodyElement.lastElementChild.children) {
+    if (!isNaN(trAmounts.textContent)) {
+      totalValue += Number(trAmounts.textContent);
     }
-
-   const tfooterElement = document.createElement("tfoot");
-   const trElement = document.createElement("tr");
-   const thElement = document.createElement("th");
-   const tdElement = document.createElement("td");
-
-   tableElement.appendChild(tfooterElement);
-   tfooterElement.appendChild(trElement);
-   trElement.appendChild(thElement);
-   trElement.appendChild(tdElement);
-
-   let totalValue = 0;
-
-   for(let trAmounts of tableElement.children[2].children){
-        totalValue += Number(trAmounts.children[1].textContent)
-   }
-
-   thElement.setAttribute("scope", "row");
-   tdElement.setAttribute("id", "total-amount");
-   thElement.innerText = col1
-   tdElement.innerText = totalValue
+  }
+  totalAmountElement.textContent = totalValue;
 }
